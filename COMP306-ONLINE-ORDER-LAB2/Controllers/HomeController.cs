c72e461280b811f3d613155cadc71765b8f4eed0
ilgulee <iglee2000ca@gmail.com>
2017-09-30 21:43:53 -0400

    Added ItemList and Details

--- COMP306-ONLINE-ORDER-LAB2/Controllers/HomeController.cs
+++ COMP306-ONLINE-ORDER-LAB2/Controllers/HomeController.cs
@@ -13,18 +13,6 @@ namespace COMP306_ONLINE_ORDER_LAB2.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
